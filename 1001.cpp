91d1cc45112fa013e62459b8a9f3e60d9076559b
Dreykson Bessa <dreyksonbessa16@hotmail.com>
2019-05-08 15:14:09 -0300

    OK

---
+++ 1001.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a, b, x;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	x=a+b;
+	system("cls");
+	printf(" x = %d \n", x);
+	
+	return 0;
+}
